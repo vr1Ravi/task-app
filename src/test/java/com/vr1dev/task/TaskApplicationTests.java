c680000eae4ec6605635dd54927ea7f56021a80b
Ravi Jha <ravikumarjha059@gmail.com>
2026-02-26 22:25:23 +0530

    task app setup

---
+++ src/test/java/com/vr1dev/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr1dev.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
